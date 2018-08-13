bd9f3ec61902e11f930b9f945f53b2a55684ef55
ming.hsu <ming.hsu@isuncloud.com>
2018-08-13 13:01:14 +0800

    Add react-native-randombytes

--- global.js
+++ global.js
@@ -1,6 +1,7 @@
 // Inject node globals into React Native global scope.
 global.Buffer = require('buffer').Buffer;
 global.process = require('process');
+let randomBytes = require('react-native-randombytes').randomBytes;
 
 if (typeof btoa === 'undefined') {
   global.btoa = function (str) {
@@ -14,29 +15,8 @@ if (typeof atob === 'undefined') {
   };
 }
 
-// Don't do this in production. You're going to want to patch in
-// https://github.com/mvayngrib/react-native-randombytes or similar.
 global.crypto = {
   randomBytes(size, cb) {
-    // phantomjs needs to throw
-    if (size > 65536) throw new Error('requested too many random bytes');
-    // in case browserify  isn't using the Uint8Array version
-    var rawBytes = new global.Uint8Array(size);
-
-    // This will not work in older browsers.
-    // See https://developer.mozilla.org/en-US/docs/Web/API/window.crypto.getRandomValues
-    if (size > 0) {
-      // getRandomValues fails on IE if size == 0
-      for (let i = 0; i < rawBytes.length; i++) {
-        rawBytes[i] = Math.floor(256 * Math.random());
-      }
-    }
-    // XXX: phantomjs doesn't like a buffer being passed here
-    var bytes = Buffer.from(rawBytes.buffer);
-
-    if (cb) {
-      cb(bytes);
-    }
-    return bytes;
+    return randomBytes(size, cb);
   }
 };
